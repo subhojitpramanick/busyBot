19bc91a14c166987851e06806a15d39ba99ff0dd
subhojitpramanick <subhojitpramanick.allwork@gmail.com>
2025-07-31 15:25:45 +0530

    cloudinary.js changes added some logs

--- server/configs/cloudinary.js
+++ server/configs/cloudinary.js
@@ -1,6 +1,19 @@
 import { v2 as cloudinary } from "cloudinary";
 
 const conectCloudinary = () => {
+  // --- Start of new logging code ---
+  console.log("--- Attempting to configure Cloudinary ---");
+  console.log(
+    "Found CLOUDINARY_CLOUD_NAME:",
+    process.env.CLOUDINARY_CLOUD_NAME
+  );
+  console.log("Found CLOUDINARY_API_KEY:", process.env.CLOUDINARY_API_KEY);
+  console.log(
+    "Found CLOUDINARY_API_SECRET:",
+    process.env.CLOUDINARY_API_SECRET ? "Exists" : "Not Found"
+  ); // Hide the secret itself
+  // --- End of new logging code ---
+
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
