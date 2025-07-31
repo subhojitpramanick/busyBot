86087d6b913c9dae4baceee98f078660425e021a
subhojitpramanick <subhojitpramanick.allwork@gmail.com>
2025-07-31 15:47:43 +0530

    more debugging

--- server/configs/cloudinary.js
+++ server/configs/cloudinary.js
@@ -3,10 +3,7 @@ import { v2 as cloudinary } from "cloudinary";
 const conectCloudinary = () => {
   // --- Start of new logging code ---
   console.log("--- Attempting to configure Cloudinary ---");
-  console.log(
-    "Found CLOUDINARY_CLOUD_NAME:",
-    process.env.CLOUDINARY_CLOUD_NAME
-  );
+  console.log("Found CLOUDINARY_CLOUD_NAME:", process.env.dvcf532au);
   console.log("Found CLOUDINARY_API_KEY:", process.env.CLOUDINARY_API_KEY);
   console.log(
     "Found CLOUDINARY_API_SECRET:",
