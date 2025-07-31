cd2362ed925d70d3910df2b0e2fb2848e60ff1e7
subhojitpramanick <subhojitpramanick.allwork@gmail.com>
2025-07-31 14:55:52 +0530

    some debugging

--- server/controllers/aicontroller.js
+++ server/controllers/aicontroller.js
@@ -43,6 +43,8 @@ export const generateArticle = async (req, res) => {
 
     await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${prompt}, ${content}, 'article')`;
 
+    console.log("Checking user plan. Plan is:", plan);
+
     if (plan !== "premium") {
       await clerkClient.users.updateUserMetadata(userId, {
         privateMetadata: {
